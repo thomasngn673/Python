6ce3a26edb409232bb158321aa2488f12bba3b29
thomasngn673 <67819863+thomasngn673@users.noreply.github.com>
2021-02-11 23:59:41 -0600

    added 3D scatter plot graphing using matplotlib

--- ieee/main.py
+++ ieee/main.py
@@ -1,12 +1,16 @@
 import pandas as pd
-# source ./env/bin/activate # command line past to activate virtual environment
-# change directory: cd /Users/thomasnguyen/Desktop/Programming/Python/ieee
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import json
+#source ./env/bin/activate # command line past to activate virtual environment
+#change directory: cd /Users/thomasnguyen/Desktop/Programming/Python/ieee
 
 print("Reading Excel file...")
 print("Year: 2017-2018...")
 print('\n')
 pd.set_option('display.max_rows',None,'display.max_columns',None) # display rows and columns up to a limit of 'None' (no limit)
-df = pd.read_excel('IEEE_Member_App_17-18.xlsx', sheet_name='Membership',usecols=['Last Name', 'First Name','Major (for statistical purposes only)', 'Classification (for statistical purposes only)','Total points'],nrows=90) # read excel file into dataframe variable 'df'
+df = pd.read_excel('IEEE_Member_App_17-18.xlsx', sheet_name='Membership',usecols=['Last Name', 'First Name','Major (for statistical purposes only)', 'Classification (for statistical purposes only)','Total points'],nrows=235) # read excel file into dataframe variable 'df'
 
 # Print name of columns that are loaded in
 cols = list(df.columns) # convert the columns into a list
@@ -22,11 +26,13 @@ sm = 0
 jn = 0
 sr = 0
 gd = 0
+nl = 0
 fm_tp = 0.0
 sm_tp = 0.0
 jn_tp = 0.0
 sr_tp = 0.0
 gd_tp = 0.0
+nl_tp = 0.0
 class_list_total = 0
 
 # Error encountered (resolved):
@@ -34,7 +40,7 @@ class_list_total = 0
 # print(type(points_list[3])) # What type of variable is 'nan'? Answer: float --> must convert element to string to use ==
 for i in range(len(points_list)):
     if str(points_list[i]) == 'nan':
-        points_list[i] = 0
+        points_list[i] = 0.0
 
 for i in range(len(class_list)):
     if class_list[i] == 'Freshman':
@@ -52,6 +58,9 @@ for i in range(len(class_list)):
     if class_list[i] == 'Graduate':
         gd = gd + 1
         gd_tp = gd_tp + points_list[i]
+    if str(class_list[i]) == 'nan':
+        nl = nl + 1
+        nl_tp = nl_tp + points_list[i]
 
 print("Number of freshmen:",fm)
 print("Number of sophomores:",sm)
@@ -62,7 +71,7 @@ print("TOTAL NUMBER OF MEMBERS:",(fm+sm+jn+sr+gd))
 print('\n')
 
 print("Total points for freshmen:",fm_tp)
-print("Total ponits for sophmores:",sm_tp)
+print("Total points for sophmores:",sm_tp)
 print("Total points for juniors:",jn_tp)
 print("Total points for seniors:",sr_tp)
 print("Total points for graduates:",gd_tp)
@@ -78,19 +87,24 @@ if max_p == sr_tp:
     most_active_class = 'SENIORS'
 if max_p == gd_tp:
     most_active_class = 'GRADUATES'
+if max_p == nl_tp:
+    most_active_class = 'NOT LISTED'
 print("MOST ACTIVE CLASS:",most_active_class)
 print('\n')
 
 # Determine membership major composition
 # https://www.geeksforgeeks.org/python-add-the-occurrence-of-each-number-as-sublists/
-l1 = df['Major (for statistical purposes only)'].to_list() # convert column of majors to a list
+major_list = df['Major (for statistical purposes only)'].to_list() # convert column of majors to a list
+for i in range(len(major_list)):
+    if str(major_list[i]) == 'nan':
+        major_list[i] = 'Not listed'
 l = [] # sub lists
 unq_l = [] # master list
 def count_occur(list1): # create function called "count_occur"
     for i in range(0, len(list1)): 
         a = 0
         row = [] # create empty list
-        if i not in l: # 'l' is empty, so this statement should loop everytime 'for i in range(0, len(l1))' is entered
+        if i not in l: # 'l' is empty, so this statement should loop everytime 'for i in range(0, len(major_list))' is entered
             for j in range(0, len(list1)): 
   
                 # matching items from both lists 
@@ -112,11 +126,14 @@ def count_occur(list1): # create function called "count_occur"
               
     return unq_l
     
-count_occur(l1) # prints list of lists that has Major type and Number of Major type
+count_occur(major_list) # prints list of lists that has Major type and Number of Major type
+unq_major_list = [] # must be created to be used in dynamic graph as different axis
 for i in range(0,len(unq_l)):
     for j in range(0,1):
         print("Number of",unq_l[i][j],"Majors:",unq_l[i][j+1])
+        unq_major_list.append(unq_l[i][0]) # only want the Major type
 print("\n")
+# print(unq_major_list,"\n") # prints list of all member's majors
 
 # Stats plan: 
 # Which major has the most points?
@@ -124,3 +141,45 @@ print("\n")
 # Create graph for general meetings (x: classification, y: major, z: # of attended meetings)
 # Create graph for event (x: classification, y: major, z: # of attended events)
 # Create graph for all event (x: classification, y: major, z: # of all attended events)
+
+# 3D Graphing
+# Convert class_list to plottable data, assign class type to a number
+plt_class_list = []
+for i in range(len(class_list)):
+    if class_list[i] == 'Freshman':
+        plt_class_list.append(1)
+    if class_list[i] == 'Sophomore':
+        plt_class_list.append(2)
+    if class_list[i] == 'Junior':
+        plt_class_list.append(3)
+    if class_list[i] == 'Senior':
+        plt_class_list.append(4)
+    if class_list[i] == 'Graduate':
+        plt_class_list.append(5)
+    if str(class_list[i]) == 'nan':
+        plt_class_list.append(6)
+
+# Convert major_list to plottable data, assign major type to a number 
+plt_major_list = []
+for i in range(len(major_list)):
+    for j in range(len(unq_major_list)):
+        if major_list[i] == unq_major_list[j]:
+            plt_major_list.append(j+1)
+
+fig = plt.figure(figsize = (12,9)) # specify figure size
+ax = plt.axes(projection ="3d")
+ax.scatter3D(plt_class_list, plt_major_list, points_list, s=100, c='green') # specify marker size and color, plot (x,y,z)
+ax.set_title('Membership Activity Classification', size=12)
+plt.xticks(fontsize=8, rotation=0)
+plt.yticks(fontsize=6, rotation=0)
+# ax.set_xticks(range(6)) # sets the boundaries of xticks                     LIBRARY ERROR CAUSES CUT OFF 
+# ax.set_xticklabels(['Freshman','Sophomore','Junior','Senior','Graduate','Not listed'])
+ax.set_yticks(range(len(unq_major_list))) # ensure that all tick marks are shown, make # of tick marks = # of unique majors
+# unq_major_list = json.dumps(unq_major_list) # adds double quotes around every element in the list
+ax.set_yticklabels(unq_major_list) # assign all unique majors to ytick labels 
+ax.set_xlabel('Classification (x)', size=10)
+ax.xaxis.labelpad = 20 # changes spacing of xlabel relative to axis
+ax.set_ylabel('Majors (y)', size=10)
+ax.yaxis.labelpad = 30 # changes spacing of ylabel relative to axis
+ax.set_zlabel('Total Points (z)', size=10)
+plt.show()
